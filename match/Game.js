e9de5e6fb8219f89d57ddfc4f78d410df91ce0e1
Andrew Leto <aleto94@gmail.com>
2016-09-02 14:38:52 -0400

    exact_fit

--- match/Game.js
+++ match/Game.js
@@ -10,7 +10,7 @@ main.prototype = {
 
     preload: function() {
 
-        this.game.scale.scalemode = Phaser.ScaleManager.SHOW_ALL;
+        this.game.scale.scalemode = Phaser.ScaleManager.EXACT_FIT;
 
         this.game.load.image('grey', 'assets/runeGrey.png');
         this.game.load.image('green', 'assets/runeGreen.png');
