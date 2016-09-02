33f01c0211763d78259542c81bf5fb9be7df113f
Andrew Leto <aleto94@gmail.com>
2016-09-02 14:35:56 -0400

    scale changed

--- match/Game.js
+++ match/Game.js
@@ -10,6 +10,8 @@ main.prototype = {
 
     preload: function() {
 
+        game.scale.scalemode = Phaser.ScaleManager.SHOW_ALL;
+
         this.game.load.image('grey', 'assets/runeGrey.png');
         this.game.load.image('green', 'assets/runeGreen.png');
         this.game.load.image('red', 'assets/runeRed.png');
