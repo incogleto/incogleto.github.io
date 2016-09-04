c5c0704e9a4cd863f02a84267d84129062381f27
Andrew Leto <aleto94@gmail.com>
2016-09-04 19:07:13 -0400

    remove setscreensize

--- match/Game.js
+++ match/Game.js
@@ -38,7 +38,7 @@ main.prototype = {
             this.scale.hasResized.add(this.gameResized, this);
             this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
             this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
-            this.scale.setScreenSize(true);
+            //this.scale.setScreenSize(true);
         }
 
     },
