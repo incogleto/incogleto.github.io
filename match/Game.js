92cf67b625a0aa408954eb7c1d479529c39e4fb6
Andrew Leto <aleto94@gmail.com>
2016-09-04 19:17:28 -0400

    comments

--- match/Game.js
+++ match/Game.js
@@ -36,8 +36,8 @@ main.prototype = {
             this.scale.pageAlignVertically = true;
             this.scale.forceOrientation(true, false);
             //this.scale.hasResized.add(this.gameResized, this);
-            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
-            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+            //this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            //this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
             //this.scale.setScreenSize(true);
         }
 
