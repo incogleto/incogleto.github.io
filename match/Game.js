515e5374c4fa4c5518676d61d45e5fd65cf2f73c
Andrew Leto <aleto94@gmail.com>
2016-09-04 22:27:03 -0400

    comment

--- match/Game.js
+++ match/Game.js
@@ -25,7 +25,7 @@ main.prototype = {
             this.scale.maxHeight = this.gameHeight;
             this.scale.pageAlignHorizontally = true;
             this.scale.pageAlignVertically = true;
-            this.scale.setScreenSize(true);
+            this.scale.refresh();
         } else {
             this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
             this.scale.minWidth = this.gameWidth / 2;
@@ -35,10 +35,10 @@ main.prototype = {
             this.scale.pageAlignHorizontally = true;
             this.scale.pageAlignVertically = true;
             this.scale.forceOrientation(true, false);
-            this.scale.hasResized.add(this.gameResized, this);
-            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
-            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
-            this.scale.setScreenSize(true);
+            //this.scale.hasResized.add(this.gameResized, this);
+            //this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            //this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
+            this.scale.refresh();
         }
 
         var ow = parseInt(this.game.canvas.style.width, 10);
@@ -50,9 +50,9 @@ main.prototype = {
         this.game.canvas.style.height = nh + "px";
         this.game.canvas.style.marginLeft = (window.innerWidth / 2 - nw / 2) + "px";
         this.game.canvas.style.marginTop = (window.innerHeight / 2 - nh / 2) + "px";
-        document.getElementById("game").style.width = window.innerWidth + "px";
-        document.getElementById("game").style.height = window.innerHeight - 1 + "px"; //The css for body includes 1px top margin, I believe this is the cause for this -1
-        document.getElementById("game").style.overflow = "hidden";
+        //document.getElementById("game").style.width = window.innerWidth + "px";
+        //document.getElementById("game").style.height = window.innerHeight - 1 + "px"; //The css for body includes 1px top margin, I believe this is the cause for this -1
+        //document.getElementById("game").style.overflow = "hidden";
 
     },
 
